fe3d4dcdd99a16616963dbb3f0a358c848eb18b3
Marine Mézin <mezin.marine@gmail.com>
2017-10-20 14:27:36 -0400

    Add files via upload

---
+++ Laboratoire2Git/CPiece.cpp
@@ -0,0 +1,129 @@
+#include "CPiece.h"
+
+
+#include <iostream>
+using namespace std;
+
+#include "CPlateau.h"
+#include "CEcran.h"
+#include "CPiece.h"
+#include "CCaseVide.h"
+
+bool CPiece::CheminVide (int incX, int incY)
+{
+	bool CheminLibre = true;
+	
+	int Dy = 0;
+	if (incY != 0) Dy =  incY / abs (incY);
+	int Dx = 0;
+	if (incX != 0) Dx =  incX / abs (incX);
+	
+	int Fini = 0;
+	int y = PosY + Dy, x = PosX + Dx;
+
+	while (CheminLibre && (y != PosY+incY || x != PosX + incX) )		
+	{
+		if (Jeu->Case(y,x)->isCoulBlanc() != 0)
+			CheminLibre = false;
+		y = y + Dy;
+		x = x + Dx;
+	}
+
+	if (CheminLibre)
+	{
+		int setCouleurArriver = Jeu->Case(PosX+incX,PosY+incY)->isCoulBlanc();
+		if (!(IsCoulBlanc == -setCouleurArriver || setCouleurArriver == 0))
+			CheminLibre = false;
+	}
+	else
+		;	// C'esti un mauvais coup
+	return CheminLibre;			
+}
+
+
+CPiece::CPiece (CPlateau* J, int PX, int PY, int V, int CB)
+{
+	Jeu			= J;
+	Valeur		= V;
+	IsCoulBlanc = CB;				// 1 = Blanc, -1 = noir
+	PosX		= PX;
+	PosY		= PY;
+}
+
+
+bool CPiece::Bouger(int incX, int incY)
+{
+	if (PosX + incX >= 0 && PosX + incX < 8 && 
+		PosY + incY >= 0 && PosY + incY < 8)
+	{
+		if (CheminVide(incX, incY))
+		{
+			Jeu->prendrePiece(PosX, PosY);
+
+			PosX = PosX + incX;
+			PosY = PosY + incY;
+
+			Jeu->deposerPiece(PosX, PosY, this);
+			return true;
+		}
+	}
+	return false;
+}
+
+
+/**
+* Change la couleur du curseur pour mettre celle de la case de fond et 
+* de la pièce
+*/
+void CPiece::setCouleur ()
+{
+	int fore = Noir, fond = Blanc;
+
+	if ((PosX+PosY) % 2 ==0)	fond = Noir;
+	else						fond = Blanc;
+
+	if (IsCoulBlanc == 1)		fore  = Rouge;
+	else						fore = Bleu;
+
+	CEcran::setCouleur (fond, fore);
+}
+
+
+void CPiece::dessineCase()
+{
+	for (int y=0; y<HauteurCase; y++)
+	{
+		CEcran::Gotoxy (PosX *LargeurCase + 1, PosY*HauteurCase + 1 + y);
+		for (int x=0; x < LargeurCase; x++)
+			cout <<(" ");
+	}
+	CEcran::Gotoxy (PosX *LargeurCase + 1 + (int)(LargeurCase/2), 
+					PosY*HauteurCase+1 + (int)(HauteurCase/2));
+}
+
+
+/********************************
+/**  modification (DEBUT)
+Cette méthode sera fausse pour toutes les pièces et sera juste redéfinie dans les pions*/
+bool CPiece::priseEnPassant()
+{
+	return false;
+}
+
+bool CPiece::Manger(int incX, int incY)
+{
+	if (PosX + incX >= 0 && PosX + incX < 8 &&
+		PosY + incY >= 0 && PosY + incY < 8)
+	{
+		Jeu->prendrePiece(PosX, PosY);
+
+		PosX = PosX + incX;
+		PosY = PosY + incY;
+
+		Jeu->deposerPiece(PosX, PosY, this);
+		return true;
+	}
+	return false;
+}
+/**  modification (FIN)
+/********************************/
---
+++ Laboratoire2Git/CPion.h
@@ -0,0 +1,51 @@
+#ifndef CCPION_H
+#define CCPION_H
+
+#include "CPiece.h"
+#include "CPlateau.h"
+
+
+/**
+  * Cette classe permet d'implanter des pions au échec.
+  */
+class CPion : public CPiece
+{
+	/********************************
+	/**  modification (DEBUT)*/
+private :
+	bool premierTour;
+	bool viensDavancer2Cases;
+	int tour; //pour compter le nommbre de tour passé depuis son premier tour
+	/**  modification (FIN)
+	/********************************/
+public:
+/**
+  * Constructeur standard.
+  */
+	CPion(CPlateau* J, int PX, int PY, int Coul);
+
+/**
+  * Il y a ici une validation spécifique pour ne permettre que les mouvement d'avant
+  * ou en diagonal pour une prise, et cela d'une seule case.
+  * incX Déplacment demandé en X.
+  * incY Déplacement demandé en Y.
+  * retourne Vrai si le déplacement a eu lieu correctement, faux sinon.
+  */
+	bool Bouger(int incX, int incY);
+
+/**
+  * Dessine un Carre de la bonne taille et y mets le bon caractère pour la pièce,
+  * de la bonne couleur.
+  */
+	void Afficher();
+
+	/********************************
+	/**  modification (DEBUT)*/
+	bool priseEnPassant();
+	/**  modification (FIN)
+	/********************************/
+};
+
+#endif
+
+
